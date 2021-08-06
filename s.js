c00a65374113e1b9f5722f50af6bdb8c66c60528
sebajuffar <seba.juffar@gmail.com>
2021-08-06 18:58:53 -0300

    Create s.js

---
+++ s.js
@@ -0,0 +1,12 @@
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://sebajuffar.github.io/s.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
